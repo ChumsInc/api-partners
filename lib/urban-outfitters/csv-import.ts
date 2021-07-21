884d1d9b9215888aa1140675d4e002e9aa40ea9e
Steve Montgomery <steve@chums.com>
2021-07-21 09:22:56 -0600

    Fix to Urban Outfitters for changes to date formats.

--- lib/urban-outfitters/csv-import.ts
+++ lib/urban-outfitters/csv-import.ts
@@ -13,6 +13,9 @@ import {ParsedCSV, SageOrder, SalesOrderDetail} from "./uo-types";
 const URBAN_ACCOUNT = process.env.URBAN_OUTFITTERS_SAGE_ACCOUNT || '01-TEST';
 const UPLOAD_PATH = '/tmp/api-partners/';
 
+const dmyRegex = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4}) - ([0-9:]+)$/i;        //example: 21/07/2021 - 09:37:10
+const mdyRegex = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4}) ([0-9:]+) (AM|PM)$/i;                     //example: 04/29/2021 03:46:10 PM
+
 export interface SageOrderList {
     [key:string]: SageOrder,
 }
@@ -32,43 +35,68 @@ async function ensureUploadPathExists() {
     }
 }
 
-
+function parseOrderDate(value:string):string {
+    try {
+        if (dmyRegex.test(value)) {
+            const parsed = dmyRegex.exec(value);
+            if (parsed) {
+                const [str, day, month, year] = parsed;
+                return new Date(Number(year), Number(month) - 1, Number(day)).toISOString();
+            }
+        } else if (mdyRegex.test(value)) {
+            const parsed = dmyRegex.exec(value);
+            if (parsed) {
+                const [str, month, day, year] = parsed;
+                return new Date(Number(year), Number(month) - 1, Number(day)).toISOString();
+            }
+        }
+    } catch(err) {
+        debug("parseDate()", {value}, err.message);
+    }
+    debug('parseOrderDate() Invalid date value', {value});
+    return value;
+}
 
 function parseOrderHeader(row:ParsedCSV):SageOrder {
-    return {
-        CustomerPONo: row['Order number'],
-        ShipExpireDate: new Date(row['Shipping deadline']).toISOString(),
-        commentText: [
-            `Date Created: ${row['Date created']}`,
-            `Shipping Method: ${row['Shipping method']}`,
-            `Shipping Deadline: ${row['Shipping deadline']}`,
-        ],
-        
-        BillToName: [row['Billing address civility'], row['Billing address first name'], row['Billing address last name']].join(' ').trim(),
-        BillToAddress1: row['Billing address street 1'] || '',
-        BillToAddress2: row['Billing address street 2'] || '',
-        BillToAddress3: row['Billing address complementary'] || '',
-        BillToCity: row['Billing address city'] || '',
-        BillToState: row['Billing address state'] || '',
-        BillToZipCode: row['Billing address zip'] || '',
-        BillToCountryCode: row['Billing address country'] || '',
+    try {
+        return {
+            CustomerPONo: row['Order number'],
+            ShipExpireDate: parseOrderDate(row['Shipping deadline']),
+            commentText: [
+                `Date Created: ${row['Date created']}`,
+                `Shipping Method: ${row['Shipping method']}`,
+                `Shipping Deadline: ${row['Shipping deadline']}`,
+            ],
 
-        ShipToName: [row['Shipping address civility'], row['Shipping address first name'], row['Shipping address last name']].join(' ').trim(),
-        ShipToAddress1: row['Shipping address street 1'] || '',
-        ShipToAddress2: row['Shipping address street 2'] || '',
-        ShipToAddress3: row['Shipping address complementary'] || '',
-        ShipToCity: row['Shipping address city'] || '',
-        ShipToState: row['Shipping address state'] || '',
-        ShipToZipCode: row['Shipping address zip'] || '',
-        ShipToCountryCode: row['Shipping address country'] || '',
+            BillToName: [row['Billing address civility'], row['Billing address first name'], row['Billing address last name']].join(' ').trim(),
+            BillToAddress1: row['Billing address street 1'] || '',
+            BillToAddress2: row['Billing address street 2'] || '',
+            BillToAddress3: row['Billing address complementary'] || '',
+            BillToCity: row['Billing address city'] || '',
+            BillToState: row['Billing address state'] || '',
+            BillToZipCode: row['Billing address zip'] || '',
+            BillToCountryCode: row['Billing address country'] || '',
 
-        SalesTaxAmt: 0, //Number(row['Total order taxes'] || 0) + Number(row['Total shipping taxes'] || 0),
-        FreightAmt: Number(row['Shipping total amount']) - Number(row['Total shipping taxes'] || 0),
-        TaxableAmt: 0,
-        OrderTotal: 0,
-        NonTaxableAmt: 0,
-        CommissionAmt: 0,
-        detail: []
+            ShipToName: [row['Shipping address civility'], row['Shipping address first name'], row['Shipping address last name']].join(' ').trim(),
+            ShipToAddress1: row['Shipping address street 1'] || '',
+            ShipToAddress2: row['Shipping address street 2'] || '',
+            ShipToAddress3: row['Shipping address complementary'] || '',
+            ShipToCity: row['Shipping address city'] || '',
+            ShipToState: row['Shipping address state'] || '',
+            ShipToZipCode: row['Shipping address zip'] || '',
+            ShipToCountryCode: row['Shipping address country'] || '',
+
+            SalesTaxAmt: 0, //Number(row['Total order taxes'] || 0) + Number(row['Total shipping taxes'] || 0),
+            FreightAmt: Number(row['Shipping total amount']) - Number(row['Total shipping taxes'] || 0),
+            TaxableAmt: 0,
+            OrderTotal: 0,
+            NonTaxableAmt: 0,
+            CommissionAmt: 0,
+            detail: []
+        }
+    } catch(err) {
+        debug("parseOrderHeader()", err.message);
+        throw new Error(err);
     }
 }
 
@@ -82,22 +110,27 @@ function parseOrderDetail(row:ParsedCSV):SalesOrderDetail {
     }
 }
 function parseOrders(rows: ParsedCSV[]):SageOrder[] {
-    const orders:SageOrderList = {};
+    try {
+        const orders:SageOrderList = {};
 
-    rows.forEach((row) => {
-        const key = row['Order number'];
+        rows.forEach((row) => {
+            const key = row['Order number'];
 
-        if (!orders[key]) {
-            orders[key] = parseOrderHeader(row);
-        }
-        const line = parseOrderDetail(row);
-        orders[key].detail.push(line);
+            if (!orders[key]) {
+                orders[key] = parseOrderHeader(row);
+            }
+            const line = parseOrderDetail(row);
+            orders[key].detail.push(line);
 
-        orders[key].TaxableAmt += line.UnitPrice * line.QuantityOrdered;
-        orders[key].OrderTotal = orders[key].TaxableAmt + orders[key].SalesTaxAmt + orders[key].FreightAmt;
-        orders[key].CommissionAmt += -1 * Number(orders[key]['Commission (excluding taxes)'] || 0)
-    });
-    return Object.values(orders);
+            orders[key].TaxableAmt += line.UnitPrice * line.QuantityOrdered;
+            orders[key].OrderTotal = orders[key].TaxableAmt + orders[key].SalesTaxAmt + orders[key].FreightAmt;
+            orders[key].CommissionAmt += -1 * Number(orders[key]['Commission (excluding taxes)'] || 0)
+        });
+        return Object.values(orders);
+    } catch(err) {
+        debug("parseOrders()", err.message);
+        throw new Error(err);
+    }
 }
 
 
@@ -143,7 +176,7 @@ async function handleUpload(req:Request, userId: number):Promise<any> {
                     const url = `https://intranet.chums.com/node-sage/api/CHI/salesorder/${URBAN_ACCOUNT}/po/:CustomerPONo`
                         .replace(':CustomerPONo', encodeURIComponent(order.CustomerPONo));
                     const {results} = await fetchGETResults(url)
-                    debug('form.parse()', results);
+                    // debug('form.parse()', results);
                     if (results.SalesOrder?.SalesOrderNo) {
                         importResults.push({error: 'Order exists', import_result: 'order already exists', ...results.SalesOrder});
                     } else {
